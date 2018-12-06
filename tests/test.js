edf03abdea046bce72303a479d55a8228df940a6
cpv123 <c.vibert@hotmail.com>
2018-12-06 21:58:53 +0000

    Update tests to include new syntax and add varying delay tests
    Each existing test has a corresponding test using the new syntax for regression purposes. Additional tests for varying the delays, where the actual delays have been measured and compared (within reason) to the expected delays.

--- tests/test.js
+++ tests/test.js
@@ -26,6 +26,10 @@ describe('superagent-retry-delay', function () {
       server = app.listen(port, done)
     })
 
+    afterEach(function () {
+      requests = 0
+    })
+
     it('should not retry on success', function (done) {
       agent
         .get('http://localhost:' + port)
@@ -33,7 +37,17 @@ describe('superagent-retry-delay', function () {
         .end(function (err, res) {
           res.text.should.eql('hello!')
           requests.should.eql(1)
+          done(err)
+        })
+    })
 
+    it('should not retry on success - multiple delays format', function (done) {
+      agent
+        .get('http://localhost:' + port)
+        .retry(5, [17, 17, 17, 17, 17])
+        .end(function (err, res) {
+          res.text.should.eql('hello!')
+          requests.should.eql(1)
           done(err)
         })
     })
@@ -62,10 +76,25 @@ describe('superagent-retry-delay', function () {
       server = app.listen(port, done)
     })
 
+    afterEach(function () {
+      requests = 0
+    })
+
     it('should not retry on handled errors', function (done) {
       agent
         .get('http://localhost:' + port)
         .retry(5, 13, [404])
+        .end(function (err, res) {
+          res.status.should.eql(404)
+          requests.should.eql(3)
+          done(err)
+        })
+    })
+
+    it('should not retry on handled errors - multiple delays format', function (done) {
+      agent
+        .get('http://localhost:' + port)
+        .retry(5, [13, 13, 13, 13, 13], [404])
         .end(function (err, res) {
           res.status.should.eql(404)
           requests.should.eql(3)
@@ -95,6 +124,10 @@ describe('superagent-retry-delay', function () {
       server = app.listen(port, done)
     })
 
+    afterEach(function () {
+      requests = 0
+    })
+
     it('should retry on errors', function (done) {
       agent
         .get('http://localhost:' + port)
@@ -108,6 +141,25 @@ describe('superagent-retry-delay', function () {
       agent
         .get('http://localhost:' + port)
         .retry(5, 17)
+        .end(function (err, res) {
+          res.text.should.eql('hello!')
+          done(err)
+        })
+    })
+
+    it('should retry on errors - multiple delays format', function (done) {
+      agent
+        .get('http://localhost:' + port)
+        .end(function (err, res) {
+          res.status.should.eql(503)
+
+          // appease eslint, do nothing with error to allow it to bubble up
+          if (err) { }
+        })
+
+      agent
+        .get('http://localhost:' + port)
+        .retry(5, [17, 17, 17, 17, 17])
         .end(function (err, res) {
           res.text.should.eql('hello!')
           done(err)
@@ -136,6 +188,10 @@ describe('superagent-retry-delay', function () {
       server = app.listen(port, done)
     })
 
+    afterEach(function () {
+      requests = 0
+    })
+
     it('should retry on errors', function (done) {
       agent
         .get('http://localhost:' + port)
@@ -149,6 +205,26 @@ describe('superagent-retry-delay', function () {
       agent
         .get('http://localhost:' + port)
         .retry(5, 13)
+        .end(function (err, res) {
+          res.text.should.eql('hello!')
+          requests.should.eql(5)
+          done(err)
+        })
+    })
+
+    it('should retry on errors - multiple delays format', function (done) {
+      agent
+        .get('http://localhost:' + port)
+        .end(function (err, res) {
+          res.status.should.eql(500)
+
+          // appease eslint, do nothing with error to allow it to bubble up
+          if (err) { }
+        })
+
+      agent
+        .get('http://localhost:' + port)
+        .retry(5, [13, 13, 13, 13, 13])
         .end(function (err, res) {
           res.text.should.eql('hello!')
           requests.should.eql(5)
@@ -178,6 +254,10 @@ describe('superagent-retry-delay', function () {
       server = app.listen(port, done)
     })
 
+    afterEach(function () {
+      requests = 0
+    })
+
     it('should retry on errors', function (done) {
       agent
         .get('http://localhost:' + port)
@@ -191,6 +271,26 @@ describe('superagent-retry-delay', function () {
       agent
         .get('http://localhost:' + port)
         .retry(5, 13)
+        .end(function (err, res) {
+          res.text.should.eql('hello!')
+          requests.should.eql(5)
+          done(err)
+        })
+    })
+
+    it('should retry on errors - multiple delays format', function (done) {
+      agent
+        .get('http://localhost:' + port)
+        .end(function (err, res) {
+          res.status.should.eql(404)
+
+          // appease eslint, do nothing with error to allow it to bubble up
+          if (err) { }
+        })
+
+      agent
+        .get('http://localhost:' + port)
+        .retry(5, [13, 13, 13, 13, 13])
         .end(function (err, res) {
           res.text.should.eql('hello!')
           requests.should.eql(5)
@@ -220,6 +320,10 @@ describe('superagent-retry-delay', function () {
       server = app.listen(port, done)
     })
 
+    afterEach(function () {
+      requests = 0
+    })
+
     it('should retry on errors', function (done) {
       agent
         .get('http://localhost:' + port)
@@ -236,6 +340,126 @@ describe('superagent-retry-delay', function () {
         .end(function (err, res) {
           res.text.should.eql('hello!')
           requests.should.eql(5)
+          done(err)
+        })
+    })
+
+    it('should retry on errors - multiple delays format', function (done) {
+      agent
+        .get('http://localhost:' + port)
+        .end(function (err, res) {
+          res.status.should.eql(401)
+
+          // appease eslint, do nothing with error to allow it to bubble up
+          if (err) { }
+        })
+
+      agent
+        .get('http://localhost:' + port)
+        .retry(5, [13, 13, 13, 13, 13])
+        .end(function (err, res) {
+          res.text.should.eql('hello!')
+          requests.should.eql(5)
+          done(err)
+        })
+    })
+
+    after(function (done) { server.close(done) })
+  })
+
+  describe('specifying different delays between retries', function () {
+    let requests = 0
+    let delays
+    let delaysMeasured = []
+    let delaysAdjusted = []
+    let start
+    let now
+    const port = 10410
+    const app = express()
+    let server
+
+    before(function (done) {
+      app.get('/', function (req, res, next) {
+        requests++
+        if (requests === 1) {
+          start = new Date().valueOf()
+        } else {
+          now = new Date().valueOf()
+          delaysMeasured.push(now - start)
+        }
+        if (requests > 5) {
+          res.send('hello!')
+        } else {
+          res.sendStatus(401)
+        }
+      })
+
+      server = app.listen(port, done)
+    })
+
+    afterEach(function () {
+      requests = 0
+      delaysMeasured = []
+      delaysAdjusted = []
+    })
+
+    it('retries using the specified delays', function (done) {
+      delays = [100, 200, 300, 400, 500]
+
+      agent
+        .get('http://localhost:' + port)
+        .retry(5, delays)
+        .end(function (err, res) {
+          res.text.should.eql('hello!')
+          requests.should.eql(6)
+          delaysMeasured.length.should.equal(5)
+
+          // Create a list of the actual delays measured between consecutive retries
+          for (let i = 0; i < delaysMeasured.length; i++) {
+            if (i === 0) {
+              delaysAdjusted[i] = delaysMeasured[i]
+            } else {
+              delaysAdjusted[i] = delaysMeasured[i] - delaysMeasured[i - 1]
+            }
+          }
+
+          // Assert that each delay measured is close to the specified delay
+          for (let i = 0; i < delaysAdjusted.length; i++) {
+            let delayDiff = delaysAdjusted[i] - delays[i]
+            delayDiff.should.be.within(0, 20)
+          }
+
+          done(err)
+        })
+    })
+
+    it('extrapolates the list of delays', function (done) {
+      delays = [100, 200, 300]
+      const expectedDelays = [100, 200, 300, 300, 300]
+
+      agent
+        .get('http://localhost:' + port)
+        .retry(5, delays)
+        .end(function (err, res) {
+          res.text.should.eql('hello!')
+          requests.should.eql(6)
+          delaysMeasured.length.should.equal(5)
+
+          // Create a list of the actual delays measured between consecutive retries
+          for (let i = 0; i < delaysMeasured.length; i++) {
+            if (i === 0) {
+              delaysAdjusted[i] = delaysMeasured[i]
+            } else {
+              delaysAdjusted[i] = delaysMeasured[i] - delaysMeasured[i - 1]
+            }
+          }
+
+          // Assert that each delay measured is close to the specified delay
+          for (let i = 0; i < delaysAdjusted.length; i++) {
+            let delayDiff = delaysAdjusted[i] - expectedDelays[i]
+            delayDiff.should.be.within(0, 20)
+          }
+
           done(err)
         })
     })
