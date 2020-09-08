1fc812fed2d783502fd7d98408ca698add8c682f
Luis Pabon <luis.pabon@auronconsulting.co.uk>
2020-09-08 14:01:06 +0100

    Cache node_modules during build test

--- Makefile
+++ Makefile
@@ -2,7 +2,7 @@
 test:
 	yarn test
 
-coverage:
+upload-coverage:
 	yarn coverage
 
 publish:
--- concourse/tests.sh
+++ concourse/tests.sh
@@ -3,14 +3,9 @@
 # Ensure we exit with failure if anything here fails
 set -e
 
-INITIAL_FOLDER=`pwd`
-
 # cd into the codebase, as per CI source
 cd code
 
 apk add make
 yarn install
-make test coverage
-
-cd ${INITIAL_FOLDER}
-
+make test upload-coverage
